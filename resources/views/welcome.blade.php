593b87666344a4930e34a8409c2bae33a8e318a8
mazharrasyad <muhazharrasyad@gmail.com>
2020-02-19 17:26:40 +0700

    change judul

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Link and Match
+                    Link and Match Proyek
                 </div>
 
                 <div class="links">
